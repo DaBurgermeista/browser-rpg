79f908ed2170e31dad4194e174bf6a775cfdd14f
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-05-01 11:57:25 -0400

    Update items.js

--- items.js
+++ items.js
@@ -2,16 +2,19 @@ window.items = {
   "Rusty Dagger": {
     name: "Rusty Dagger",
     slot: "weapon",
-    bonuses: { attackSpeed: -300, dexterity: +1 }
+    bonuses: { attackSpeed: -300, dexterity: +1 },
+    tooltip: "A dull dagger that still gets the job done.\n-300ms attack delay\n+1 Dexterity"
   },
   "Tattered Cloak": {
     name: "Tattered Cloak",
     slot: "armor",
-    bonuses: { regen: +0.1 }
+    bonuses: { regen: +0.1 },
+    tooltip: "Barely holds together, but it helps.\n+0.1 HP regen"
   },
   "Lucky Coin": {
     name: "Lucky Coin",
     slot: "accessory",
-    bonuses: { dexterity: +2 }
+    bonuses: { dexterity: +2 },
+    tooltip: "Feels lucky!"
   }
 };
