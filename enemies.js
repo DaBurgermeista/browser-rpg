863931f995cde402065a41b125514c40fce71f12
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-05-01 08:41:29 -0400

    Create enemies.js

---
+++ enemies.js
@@ -0,0 +1,6 @@
+const enemies = [
+  { name: "Goblin", minDamage: 3, maxDamage: 8 },
+  { name: "Skeleton", minDamage: 5, maxDamage: 10 },
+  { name: "Slime", minDamage: 1, maxDamage: 5 },
+  { name: "Bandit", minDamage: 4, maxDamage: 12 }
+];
