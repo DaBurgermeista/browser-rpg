d99b524a780250350c481c497d0730530c4bd2e9
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-05-01 12:58:28 -0400

    Update items.js

--- items.js
+++ items.js
@@ -3,10 +3,10 @@ const items = {
     name: "Rusty Dagger",
     slot: "weapon",
     bonuses: {
+      damageRange: [1, 4],
       attackSpeed: -300,
       dexterity: +1
     },
-    damageRange: [5, 10],
     description: `A dull dagger that still gets the job done.<br>
       <span class="bonus">-300ms attack delay</span><br>
       <span class="bonus">+1 Dexterity</span>`
