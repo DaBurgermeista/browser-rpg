3653e3a93c6c7c3226f6406210c489ddb8bbd834
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-05-01 13:21:36 -0400

    Create locations.js

---
+++ locations.js
@@ -0,0 +1,21 @@
+// locations.js
+
+window.locations = {
+  town: {
+    name: "Town Square",
+    description: "A peaceful village hub with shops and taverns.",
+    actions: ["rest", "shop"]
+  },
+  woods: {
+    name: "Whispering Woods",
+    description: "Dense forest filled with wild creatures.",
+    actions: ["explore", "fight"],
+    encounters: ["Slime", "Wolf", "Bandit"]
+  },
+  dungeon: {
+    name: "Forgotten Crypt",
+    description: "A dangerous, dark place crawling with monsters.",
+    actions: ["fight"],
+    encounters: ["Skeleton", "Dark Mage"]
+  }
+};
