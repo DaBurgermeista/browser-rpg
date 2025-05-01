04a84cb49cf120337cdd0f5e983a7231ae55f171
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-05-01 08:47:33 -0400

    Update enemies.js
    Update enemy definitions

--- enemies.js
+++ enemies.js
@@ -1,6 +1,7 @@
 const enemies = [
-  { name: "Goblin", minDamage: 3, maxDamage: 8 },
-  { name: "Skeleton", minDamage: 5, maxDamage: 10 },
-  { name: "Slime", minDamage: 1, maxDamage: 5 },
-  { name: "Bandit", minDamage: 4, maxDamage: 12 }
+  { name: "Goblin", hp: 30, minDamage: 3, maxDamage: 8, attackPerSec: 1500 },
+  { name: "Skeleton", hp: 40, minDamage: 5, maxDamage: 10, attackPerSec: 2000 },
+  { name: "Slime", hp: 25, minDamage: 1, maxDamage: 5, attackPerSec: 2500 },
+  { name: "Bandit", hp: 50, minDamage: 4, maxDamage: 12, attackPerSec: 1800 }
 ];
+
