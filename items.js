b95a9f52e02c84fbb5e00f967066574235e8f8c6
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-05-01 12:39:28 -0400

    Update items.js

--- items.js
+++ items.js
@@ -1,27 +1,34 @@
-window.items = {
+const items = {
   "Rusty Dagger": {
     name: "Rusty Dagger",
     slot: "weapon",
     bonuses: {
       attackSpeed: -300,
-      dexterity: +1,
-      damageRange: [1, 3],
+      dexterity: +1
     },
+    damageRange: [5, 10],
     tooltip: `A dull dagger that still gets the job done.<br>
       <span class="bonus">-300ms attack delay</span><br>
-      <span class="bonus">+1 Dexterity</span><br>
+      <span class="bonus">+1 Dexterity</span>`
   },
 
   "Tattered Cloak": {
     name: "Tattered Cloak",
     slot: "armor",
-    bonuses: { regen: +0.1 },
-    tooltip: "Barely holds together, but it helps.\n+0.1 HP regen"
+    bonuses: {
+      regen: +0.1
+    },
+    tooltip: `Worn and threadbare, but still offers some comfort.<br>
+      <span class="bonus">+0.1 HP regen per tick</span>`
   },
+
   "Lucky Coin": {
     name: "Lucky Coin",
     slot: "accessory",
-    bonuses: { dexterity: +2 },
-    tooltip: "Shiny and oddly inspiring.\n+2 Dexterity"
+    bonuses: {
+      dexterity: +2
+    },
+    tooltip: `A shiny coin that seems to bring good fortune.<br>
+      <span class="bonus">+2 Dexterity</span>`
   }
 };
