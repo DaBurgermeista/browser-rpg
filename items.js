8b0febfdbf5b2525d40a2be7d6916336f839121a
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-05-01 09:23:43 -0400

    Create items.js

---
+++ items.js
@@ -0,0 +1,14 @@
+const items = {
+  "Rusty Dagger": {
+    slot: "weapon",
+    bonuses: { attackSpeed: -300, dexterity: +1 }
+  },
+  "Tattered Cloak": {
+    slot: "armor",
+    bonuses: { regen: +0.1 }
+  },
+  "Lucky Coin": {
+    slot: "accessory",
+    bonuses: { dexterity: +2 }
+  }
+};
